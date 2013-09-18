5f39cf6125350929ba4e103edcffe9ff58fabbb4
8indaas <8indaas@gmail.com>
2013-09-17 22:02:14 -0700

    deleted files

--- src/com/codepath/upkar/twitterapp/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.codepath.upkar.twitterapp;
-
-public final class Constants {
-	public static final int REQUEST_CODE = 10;
-	public static final String TWEET_DATA_NAME = "TweetData";
-	protected static final String USER_NAME = "UserName";
-}
